3b9223fed4d69316cbbe8028d9710681a553ef57
voratham.s <voratham.s@linecorp.com>
2024-11-19 22:48:45 +0700

    chore: adoption with match expression

--- NOTE.md
+++ NOTE.md
@@ -17,8 +17,41 @@ match divide(1.0, 0.0) {
 
 ```
 
-## Exercise Result Enum
+
+## Tutorials
+```rs
+
+match divide(5.0, 0.1) {
+        Ok(value) => println!("Result :{:#?}  ", value),
+        Err(error) => println!("Error :{:#?}  ", error),
+    }
+
+    match validate_email(String::from("dream-dream.com")) {
+        //  .. is mean don't care about the value
+        Ok(..) => println!("Email is valid"),
+        Err(reason_this_failed_validation) => println!("{}", reason_this_failed_validation),
+}
+
+fn divide(a: f64, b: f64) -> Result<f64, Error> {
+    if b == 0.0 {
+        Err(Error::other("cant divide by 0"))
+    } else {
+        Ok(a / b)
+    }
+}
+
+fn validate_email(email: String) -> Result<(), Error> {
+    if email.contains("@") {
+        Ok(())
+    } else {
+        Err(Error::other("emails must have an @"))
+    }
+}
+
 ```
+
+## Exercise Result Enum
+```rs
 use std::io::Error;
 
 // Todo: add in a return type
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,8 @@
-use std::{fs, io::Error};
+use std::fs;
 
 fn main() {
-    let text = fs::read_to_string("logs.txt");
-
-    println!("{:?}", text);
-
-    match divide(5.0, 0.1) {
-        Ok(value) => println!("Result :{:#?}  ", value),
-        Err(error) => println!("Error :{:#?}  ", error),
-    }
-
-    match validate_email(String::from("dream-dream.com")) {
-        //  .. is mean don't care about the value
-        Ok(..) => println!("Email is valid"),
-        Err(reason_this_failed_validation) => println!("{}", reason_this_failed_validation),
-    }
-}
-
-fn divide(a: f64, b: f64) -> Result<f64, Error> {
-    if b == 0.0 {
-        Err(Error::other("cant divide by 0"))
-    } else {
-        Ok(a / b)
-    }
-}
-
-fn validate_email(email: String) -> Result<(), Error> {
-    if email.contains("@") {
-        Ok(())
-    } else {
-        Err(Error::other("emails must have an @"))
+    match fs::read_to_string("logs.txt") {
+        Ok(text_that_was_read) => println!("🟢 All read file length: {}", text_that_was_read.len()),
+        Err(why_this_failed) => println!("🔴 Failed to read file: {}", why_this_failed),
     }
 }
