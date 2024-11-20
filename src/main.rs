3a0e2cf405497ce4ecb3d9bfcd028093a2b7e337
voratham.s <voratham.s@linecorp.com>
2024-11-20 22:29:44 +0700

    chore: refactor athernatives to nested matches

--- src/main.rs
+++ src/main.rs
@@ -16,23 +16,27 @@ fn extract_errors(text: &str) -> Vec<String> {
 }
 
 fn main() {
+    let text = fs::read_to_string("./asdf/logs.txt").expect("🔴 Failed to read logs.txt");
+
+    let error_logs = extract_errors(text.as_str());
+
+    fs::write("./asdf/errors.txt", error_logs.join("\n")).expect("🔴 Failed to write errors.txt");
+
     // let mut error_logs = vec![];
 
-    match fs::read_to_string("logs.txt") {
-        Ok(text_that_was_read) => {
-            println!("🟢 All read file length: {}", text_that_was_read.len());
-            let error_logs = extract_errors(text_that_was_read.as_str());
-            
-            // fs:write cannot open directory other than current directory
-            match fs::write("./asdf/errors.text", error_logs.join("\n")) {
-                Ok(()) => println!("🧑‍🔧 Wrote errors.text done ✅"),
-                Err(reason_write_failed) => {
-                    println!("🔴 Writing of errors.text failed {} ", reason_write_failed)
-                }
-            }
-        }
-        Err(why_this_failed) => println!("🔴 Failed to read file: {}", why_this_failed),
-    }
+    // match fs::read_to_string("logs.txt") {
+    //     Ok(text_that_was_read) => {
+    //         println!("🟢 All read file length: {}", text_that_was_read.len());
+    //         let error_logs = extract_errors(text_that_was_read.as_str());
 
-    
+    //         // fs:write cannot open directory other than current directory
+    //         match fs::write("./asdf/errors.text", error_logs.join("\n")) {
+    //             Ok(()) => println!("🧑‍🔧 Wrote errors.text done ✅"),
+    //             Err(reason_write_failed) => {
+    //                 println!("🔴 Writing of errors.text failed {} ",  reason_write_failed)
+    //             }
+    //         }
+    //     }
+    //     Err(why_this_failed) => println!("🔴 Failed to read file: {}", why_this_failed),
+    // }
 }
